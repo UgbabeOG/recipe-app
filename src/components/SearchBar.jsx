26a5cac59f71e405eda2919f8133b59cc5049687
UgbabeOG <ugbabe.og@gmail.com>
2023-10-29 16:32:20 -0800

    removed redundant css

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,25 +9,22 @@ const SearchBar = () => {
     <>
       <div>
         <form
-          onSubmit={(e) =>
-            search.length >= 1
-              ? getQuery(e)
-              : null
-          }
+          onSubmit={(e) => (search.length >= 1 ? getQuery(e) : null)}
           className="p-5 flex justify-center min-h-[10vh] w-full"
           action=""
         >
           <input
-            className="w-2/5 px-3 py-1 rounded shadow appearance-none placeholder:italic placeholder: bg-slate-300 outline-[#F78B3A] dark:outline-[#FB9D4D]"
+            className="w-2/5 px-3 py-1 rounded shadow  placeholder:italic placeholder:animate-pulse bg-slate-300 outline-[#F78B3A] dark:outline-[#FB9D4D]"
             placeholder="search for your recipe..."
             type="text"
             value={search}
             onChange={updateSearch}
           />
-          <button 
-          disabled={search.length <= 0 ? true:false}
-          
-          type="submit" className="p-2 rounded  active:bg-slate-300 bg-slate-200">
+          <button
+            disabled={search.length <= 0 ? true : false}
+            type="submit"
+            className="p-2 rounded  active:bg-slate-300 bg-slate-200"
+          >
             <FaSearch className="text-3xl text-[#F78B3A] dark:text-[#FB9D4D]" />
           </button>
         </form>
