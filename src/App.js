bf4d90845124e6d01353070731385a2ab1015efa
UgbabeOG <ugbabe.og@gmail.com>
2024-01-18 21:01:53 +0100

    lazy loading recipe cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { useAuth } from "./context/SearchContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SearchBar from "./components/SearchBar";
-import RecipeCard from "./components/RecipeCard";
-import { useAuth } from "./context/SearchContext";
+const SearchBar = lazy(() => import("./components/SearchBar"));
+const RecipeCard = lazy(() => import("./components/RecipeCard"));
 
 export default function App() {
   const { darkMode } = useAuth();
   return (
-    <div className={darkMode ? "dark" : "bg-slate-50 h-screen"}>
-      <Header />
-      <SearchBar />
-      <RecipeCard />
-      <Footer />
-    </div>
+    <Suspense fallback={<div className="custom-loader"></div>}>
+      <div className={darkMode ? "dark" : "bg-slate-50 h-screen"}>
+        <Header />
+        <SearchBar /> <RecipeCard />
+        <Footer />
+      </div>
+    </Suspense>
   );
 }
